b935a613a812dc448f2de8b23cec7d40f8a9a102
vellampallikrishnasaigifhub <krishnasaivellampalli@gmail.com>
2024-06-27 11:04:21 +0530

    Update base.py

--- base.py
+++ base.py
@@ -12,3 +12,17 @@ def number(a):
 number(7893)
 number(-467)
 number(0)
+#find sum of two numbers
+num1 = lamdba a,b:a+b
+print(num1(5,89)
+#find prime or not
+def prim(gh):
+count=1
+    for i in range(1,gh):
+        if(a%i)== 0:
+            count +=1
+            break
+if count == 2:
+    print(gh,"prime")
+else:
+    print(gh,"not prime")
